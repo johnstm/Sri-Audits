1972717ed3f71d883dd4960d77a9cbd0433f8c53
Gowri Lakshmi D <gowri@terrificminds.com>
2023-01-05 22:20:26 +0530

    Useful links page added

---
+++ page-links.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Template Name: Links Page
+ */
+get_header();
+?>
+    <section class="contentwrapper linkswrapper">
+        <h3 class="sa__about-who  main-heading" >Useful Links</h3>
+        <div class="sa__team-details sa__links">
+            <h4 >Professional Institutes</h4>
+            <ul>
+                <li><a href="https://icai.org/" target="_blank" class="sa__directions-address paragraphs">Institute of Chartered Accountants of India</a></li>
+                <li><a href="http://www.icwai.org/" target="_blank" class="sa__directions-address paragraphs">Institute of Costs & Works Accountants of India</a></li>
+                <li><a href="http://www.icsi.in/" target="_blank" class="sa__directions-address paragraphs">Institute of Company Secretaries of India</a></li>
+            </ul>
+        </div>
+        <div class="sa__team-details sa__links">
+            <h4>India Government Sites</h4>
+            <ul>
+                <li><a href="https://www.rbi.org.in/" target="_blank" class="sa__directions-address paragraphs">Reserve Bank of India</a></li>
+                    <ul>
+                        <li><a href="https://www.rbi.org.in/scripts/NotificationUser.aspx" target="_blank" class="sa__directions-address paragraphs">RBI Notifications</a></li>
+                        <li><a href="https://www.rbi.org.in/SCRIPTs/BS_PressreleaseDisplay.aspx" target="_blank" class="sa__directions-address paragraphs">RBI Press Releases</a></li>
+                    </ul>
+                <li><a href="https://www.mca.gov.in/content/mca/global/en/home.html" target="_blank" class="sa__directions-address paragraphs">Ministry of Corporate Affairs</a></li>
+                <li class="sa__directions-address paragraphs">Central Board of Direct Taxes</li>
+                <ul>
+                        <li><a href="https://incometaxindia.gov.in/home.asp" target="_blank" class="sa__directions-address paragraphs">Income Tax India</a></li>
+                        <li><a href="https://incometaxindiaefiling.gov.in/portal/index.do" target="_blank" class="sa__directions-address paragraphs">Income Tax e-filing portal</a></li>
+                        <li><a href="https://itat.gov.in/" target="_blank" class="sa__directions-address paragraphs">ITAT online</a></li>
+                    </ul>
+                <li><a href="http://www.servicetax.gov.in/" target="_blank" class="sa__directions-address paragraphs">Directorate of Service Tax</a></li>
+                <li><a href="https://cag.gov.in/en" target="_blank" class="sa__directions-address paragraphs">The CAG of India</a></li>
+                <li><a href="https://main.sci.gov.in/" target="_blank" class="sa__directions-address paragraphs">Supreme Court of India</a></li>
+                <li><a href="http://www.sebi.gov.in/sebiweb/" target="_blank" class="sa__directions-address paragraphs">Securities and Exchange Board of India (SEBI)</a></li>
+                <li><a href="https://www.nic.in/" target="_blank" class="sa__directions-address paragraphs">National Informatics Centre (various sites of Government of India)</a></li>
+                <li><a href="https://www.mospi.gov.in/" target="_blank" class="sa__directions-address paragraphs">Central Statistical Organisation</a></li>
+            </ul>
+        </div>
+        <div class="sa__team-details sa__links">
+            <h4>Others</h4>
+            <ul>
+                <li><a href="https://www.oanda.com/bvi-en/" target="_blank" class="sa__directions-address paragraphs">Foreign Exchange Rates</a></li>
+            <ul>
+    </section>
+<?php
+get_footer();
+?>
